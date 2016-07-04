c3c53d0f5ab88f2a9a113c20eb2ff767d8b29722
Red Fox <redfox9966@gmail.com>
2016-07-04 18:25:13 +0300

    LawController added

--- Webinars/Webinars/Controllers/LawController.cs
+++ Webinars/Webinars/Controllers/LawController.cs
@@ -3,20 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Webinars.DAL.Context;
+using Webinars.DAL.Core;
+using Webinars.DAL.Model;
 using Webinars.DAL.Repositories;
 
 namespace Webinars.Controllers
 {
     public class LawController : Controller
     {
-        private LawRepository repo;
+        private IUnitOfWork repo;
          
         public LawController()
         {
-            repo = new LawRepository();
+            repo = new UnitOfWork(new WebinarContext());
         }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
         public JsonResult GetAllLaws()
         {
+            return Json(repo.lawRepository.GetAll(), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult Get(int id)
+        {
+            return Json(repo.lawRepository.Get(id), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public void Delete(int id)
+        {
+            var entity = repo.lawRepository.Get(id);
+            repo.lawRepository.Delete(entity);
+        }
+
+        [HttpPost]
+        public void Edit(Law model)
+        {
+            repo.lawRepository.Update(model);
+        }
+
+        [HttpPost]
+        public void Create(Law model)
+        {
+            repo.lawRepository.Add(model);
         }
     }
 }
