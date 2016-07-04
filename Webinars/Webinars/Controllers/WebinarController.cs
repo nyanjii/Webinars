f1c0fce6037223e2ad7f9fbfa16557d90566ece3
nyanjii <khiv.natalia@gmail.com>
2016-07-04 18:01:13 +0300

    Webinar Controller (temporarily empty)

---
+++ Webinars/Webinars/Controllers/WebinarController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Webinars.Controllers
+{
+    public class WebinarController : Controller
+    {
+        WebinarController()
+        {
+
+        }
+        // GET: Webinar
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+
+    }
+}
