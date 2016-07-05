5076bba920462dd27133af333f065cad3ed0b2ce
nyanjii <khiv.natalia@gmail.com>
2016-07-05 12:32:39 +0300

    WebinarsViewModel

---
+++ Webinars/Webinars/Scripts/AppScripts/WebinarViewModel.js
@@ -0,0 +1,16 @@
+﻿WebinarViewModel = function () {
+    this.webinarCollection = WebinarCollectionViewModel
+}
+
+WebinarCollectionViewModel = function () {
+    this.webinars = ko.observableArray();
+}
+
+
+$(document).ready(function () {
+    ko.applyBindings(WebinarViewModel);
+    $.get('/Webinar/GetAllWebinars', function (data) {
+        ko.mapping.fromJS(data, webinars);
+    });
+
+});
