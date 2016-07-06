b517f8244ad7a6e24864e5935c34d2624427f68b
nyanjii <khiv.natalia@gmail.com>
2016-07-06 16:30:22 +0300

    Video list template + ViewModel VideoImageUrl

--- Webinars/Webinars/Scripts/AppScripts/LawWebinarsViewModel.js
+++ Webinars/Webinars/Scripts/AppScripts/LawWebinarsViewModel.js
@@ -8,9 +8,19 @@
             self.lawsArray(res());
         });
     };
-    self.GetAllWebinarsByLaw = function () {
-        $.get('/Home/GetWebinarsByLaw', {id: this.Id}, function (data) {
+    self.GetAllWebinarsByLaw = function (data) {
+        $.get('/Home/GetWebinarsByLaw', { id: data.Id() }, function (data) {
             var res = ko.mapping.fromJS(data)
+            $.each(res(), function (index, value) {
+                console.log(value);
+                value.VideoImageUrl = ko.computed(function () {
+                    url = this.VideoUrl();
+                    var start = url.indexOf("embed/") + 6;
+                    var videoId = url.substring(start, url.length);
+                    var result = "http://img.youtube.com/vi/" + videoId + "/1.jpg";
+                    return result;
+                }, value);
+            });
             self.currentWebinarsArray(res());
         });
     };
@@ -27,15 +37,17 @@
     };
     return {
         laws: self.lawsArray,
-        webinars: self.webinarsArray,
+        webinars: self.currentWebinarsArray,
         getAllLaws: self.GetAllLaws,
         getAllWebinars: self.GetAllWebinarsByLaw,
         webinar: self.chosenWebinar,
-        playWebinar: self.PlayWebinar
+        playWebinar: self.PlayWebinar,
+        getVideoUrlImage: self.GetVideoUrlImage
     };
 }()
 
 $(document).ready(function () {
     ko.applyBindings(LawWebinarsViewModel);
     LawWebinarsViewModel.getAllLaws();
+    LawWebinarsViewModel.getAllWebinars(2);
 });
