502b7d945f8329ae79044bd074faa7314dc0db7f
nyanjii <khiv.natalia@gmail.com>
2016-07-06 12:19:25 +0300

    premerge

--- Webinars/Webinars/Controllers/HomeController.cs
+++ Webinars/Webinars/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using Webinars.DAL.Context;
 using Webinars.DAL.Core;
 using Webinars.DAL.Repositories;
